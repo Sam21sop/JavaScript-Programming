ea1c9723bbdb79dd53bb73c78014a2a90737a2e6
Sam21sop <sbussiness21@gmail.com>
2024-02-08 23:22:19 +0530

    intruduction

---
+++ 00_Introduction.js
@@ -0,0 +1,115 @@
+/* 
+What is javascript
+
+- Programming Language
+- Scripting 
+- enable to change conent dynamically
+*/
+
+
+/*
+Javascript Statemenr are composed of 
+
+1. values
+2. operators
+3. Expression 
+4. Keywords
+5. Comments
+*/
+
+
+/*
+Javascript Comment
+
+1. Single line comment ==> //
+2. Multiline comment ==> /* *\/
+*/
+
+
+/*
+Javascript Variables
+
+variable are container to store data
+1. var
+2. let
+3. const
+4. atomatically
+*/
+
+
+/*
+- let 
+
+1. have block scope
+2. you can declare it before used
+3. can not be redeclared in the same scope
+*/
+
+
+/*
+- const
+
+1. con not redeclare
+2. can not reassigned
+3. have block scope
+*/
+
+
+/*
+Javascript Operator
+
+1. arithmetic (+, -, *, /, **, %, ++, --)
+2. assignment (=, +=, -=, *=, /=, %=, **=)
+3. comparision (==, ===, !=, !==, >, <, >=, <=)
+4. logical (&&, ||, !) 
+5. bitwise (&, |, ~, ^, <<, >>, >>>)
+6. ternary (?)
+7. type (typeof, instanceof)
+*/
+
+
+/*
+Javascript Data Types
+
+1. String
+2. Number
+3. Boolean
+4. Undefined
+5. Null
+6. Object
+7. Symbol
+8. Bigint(rarely use case)
+*/
+
+
+/*
+Javascript Function
+
+- Function block of code designed to perform a particular task again and again
+- code reusablility
+- DRY principal
+
+- parameter listed inside the function defination
+- arguments are actual values recieved by function when it id invoked
+*/
+
+
+/*
+Javascript object
+
+- object is collections of Properties
+- property is association between key:value pair
+- object can also have method
+- Method are action that can be perform on object
+*/
+
+
+/*
+Javascript String
+
+- string are storing for text data type
+- you can use single('') or double("") quotes to declare js string
+- template string (``) backticks
+- template string allow multiline strings
+*/
+
